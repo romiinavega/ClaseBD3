9e212a872e79c6c3cf5e28fd0c24718be7e5a9c5
romiinavega <romiivega3005@gmail.com>
2021-02-02 11:36:53 -0700

    1

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('notas', function (){
+    $notas = [
+       /* 'Primera nota',
+        'Segunda nota',
+        'Tercera nota',
+        'Cuarta nota',
+        'Quinta nota',*/
+    ];
+
+    return view('notas', ['notas' => $notas]);
+});
+
+Route::get('agregar', function (){
+    return view('agregar');
+});
